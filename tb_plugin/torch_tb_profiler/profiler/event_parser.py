0f48ada1a1c4d63bc471145aab19b571c9c9abd7
Mike Guo <myguo@microsoft.com>
2021-11-29 08:37:06 +0800

    replace backslash with parentheses

--- tb_plugin/torch_tb_profiler/profiler/event_parser.py
+++ tb_plugin/torch_tb_profiler/profiler/event_parser.py
@@ -243,8 +243,8 @@ class StepParser:
             self.role_ranges[ProfileRole.Memset].append((ts, ts + dur))
         elif evt_type == EventTypes.RUNTIME:
             self.role_ranges[ProfileRole.Runtime].append((ts, ts + dur))
-        elif evt_type == EventTypes.OPERATOR and event.name.startswith("enumerate(DataLoader)#") \
-                and event.name.endswith(".__next__"):
+        elif (evt_type == EventTypes.OPERATOR and event.name.startswith("enumerate(DataLoader)#")
+                and event.name.endswith(".__next__")):
             self.role_ranges[ProfileRole.DataLoader].append((ts, ts + dur))
         elif event.type == EventTypes.PROFILER_STEP:
             self.steps.append((ts, ts + dur))
